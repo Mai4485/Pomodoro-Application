a316db473419a056128f3d10be38902876281c6c
Akexorcist <akexorcist@gmail.com>
2025-09-01 03:40:30 +0700

    Workshop Part 1 - Add view binding
    # Conflicts:
    #	app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/MainActivity.kt

--- app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/MainActivity.kt
+++ app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.skooldio.android.fundamentals.workshop.pomodoro.config.PomodoroConfig
+import com.skooldio.android.fundamentals.workshop.pomodoro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     // Requester for permission requesting
@@ -34,12 +35,16 @@ class MainActivity : AppCompatActivity() {
         private const val LONG_BREAK_DURATION_MAX = PomodoroConfig.LONG_BREAK_DURATION_MAX
     }
 
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Additional code to support edge-to-edge in Android 15
         // Leave this code above the setContentView method
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         // Additional code to support edge-to-edge in Android 15
         // Leave this code below the setContentView method
